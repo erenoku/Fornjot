7b5981a07f5bda15b2edbc1b9535a64d5839d686
Hanno Braun <hanno@braun-odw.eu>
2022-05-06 14:46:28 +0200

    WIP Add `fj::Union`

--- crates/fj-operations/src/lib.rs
+++ crates/fj-operations/src/lib.rs
@@ -24,6 +24,7 @@ mod group;
 mod sketch;
 mod sweep;
 mod transform;
+mod union;
 
 use fj_interop::debug::DebugInfo;
 use fj_kernel::{
@@ -80,6 +81,7 @@ macro_rules! dispatch {
                         Self::Group(shape) => shape.$method($($arg_name,)*),
                         Self::Sweep(shape) => shape.$method($($arg_name,)*),
                         Self::Transform(shape) => shape.$method($($arg_name,)*),
+                        Self::Union(shape) => shape.$method($($arg_name,)*),
                     }
                 }
             )*
